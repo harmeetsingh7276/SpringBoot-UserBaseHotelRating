8dceb5120e93771d61080aa65a24c4cce90bdf99
harmeet.gorowada <harmeet.gorowada@techbulls.com>
2023-03-21 14:23:10 +0530

    V1 User microservice

---
+++ UserService/src/main/java/com/lcwd/user/service/controller/UserController.java
@@ -0,0 +1,39 @@
+package com.lcwd.user.service.controller;
+
+import com.lcwd.user.service.entities.User;
+import com.lcwd.user.service.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    //create
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User userData = userService.saveUser(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userData);
+    }
+
+    //single user
+    @GetMapping("/{userId}")
+    public ResponseEntity<User> getSingleUser(@PathVariable String userId) {
+        User userData = userService.getUser(userId);
+        return ResponseEntity.ok(userData);
+    }
+
+    //allUser
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUser() {
+        List<User> userData = userService.getAllUser();
+        return ResponseEntity.ok(userData);
+    }
+}
---
+++ UserService/src/main/java/com/lcwd/user/service/services/UserService.java
@@ -0,0 +1,19 @@
+package com.lcwd.user.service.services;
+
+import com.lcwd.user.service.entities.User;
+
+import java.util.List;
+
+public interface UserService {
+    //user Operations
+    //create
+    User saveUser(User user);
+    //getAll Users
+    List<User> getAllUser();
+
+    //get specificed UserID
+    User getUser(String userId);
+    
+    // TODO: 21/03/23  delete
+    // TODO: 21/03/23 update
+}
