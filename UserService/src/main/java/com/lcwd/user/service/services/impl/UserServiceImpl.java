d3431bb166790330d3fef98f943d985964fff69a
harmeet.gorowada <harmeet.gorowada@techbulls.com>
2023-03-22 17:44:51 +0530

    combined user and ratings service to get all the ratings entered by a user from id

--- UserService/src/main/java/com/lcwd/user/service/services/impl/UserServiceImpl.java
+++ UserService/src/main/java/com/lcwd/user/service/services/impl/UserServiceImpl.java
@@ -1,13 +1,18 @@
 package com.lcwd.user.service.services.impl;
 
+import com.lcwd.user.service.entities.Rating;
 import com.lcwd.user.service.entities.User;
 import com.lcwd.user.service.exception.ResourceNotFoundException;
 import com.lcwd.user.service.repositories.UserRepository;
 import com.lcwd.user.service.services.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.lang.module.ResolutionException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 
@@ -15,6 +20,11 @@ import java.util.UUID;
 public class UserServiceImpl implements UserService {
     @Autowired
     private UserRepository userRepository;
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+    private Logger logger = LoggerFactory.getLogger(UserServiceImpl.class);
 
     @Override
     public User saveUser(User user) {
@@ -34,7 +44,10 @@ public class UserServiceImpl implements UserService {
         User user = userRepository.findById(userId).orElseThrow(() -> new ResolutionException("User not found on server with id:" + userId));
         //populating ratings var
         //fetching ratings from ratings table for that user id
-
+        //http://localhost:8083/ratings/users/2fb51a39-9f61-4cfa-a960-132bd1ab1998
+        ArrayList<Rating> ratingsForUser = restTemplate.getForObject("http://localhost:8083/ratings/users/" + user.getUserId(), ArrayList.class);
+        logger.info("List:{}", ratingsForUser);
+        user.setRating(ratingsForUser);
         return user;
     }
 
