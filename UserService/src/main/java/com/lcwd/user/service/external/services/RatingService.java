150ce38140189190fcf111b45ee7f6090d6ae1d8
harmeet.gorowada <harmeet.gorowada@techbulls.com>
2023-03-23 15:18:55 +0530

    Added changes related to usuage of feign client

---
+++ UserService/src/main/java/com/lcwd/user/service/external/services/RatingService.java
@@ -0,0 +1,22 @@
+package com.lcwd.user.service.external.services;
+
+import com.lcwd.user.service.entities.Rating;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+
+@FeignClient(name="RATING-SERVICE")
+public interface RatingService {
+    //create ratings
+    @PostMapping("/ratings")
+    ResponseEntity<Rating> createRating(Rating values);
+    //update ratings
+    @PutMapping("/ratings/{ratingId}")
+    ResponseEntity<Rating> updateRating(@PathVariable String ratingId, Rating values);
+    //delete
+    @DeleteMapping("/ratings/{ratingId}")
+    void deleteRating(@PathVariable String ratingId);
+}
--- UserService/src/test/java/com/lcwd/user/service/UserServiceApplicationTests.java
+++ UserService/src/test/java/com/lcwd/user/service/UserServiceApplicationTests.java
@@ -1,13 +1,38 @@
 package com.lcwd.user.service;
 
+import com.lcwd.user.service.external.services.RatingService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.stereotype.Service;
 
+@Service
 @SpringBootTest
 class UserServiceApplicationTests {
+    @Autowired
+    private RatingService ratingService;
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
+    //    @Test
+//    void createRating() {
+//        Rating rating = Rating.builder().rating(10).userId("2fb51a39-9f61-4cfa-a960-132bd1ab1998").hotelId("99b192b8-4cf5-4b9d-b920-e526505a9526").feedback("Created using feignClient").build();
+//        ResponseEntity<Rating> ratingResponse=ratingService.createRating(rating);
+//		System.out.println(ratingResponse.getStatusCode()+ratingResponse.getBody().toString());
+//    }
+
+//    @Test
+//    void updateRating() {
+//        Rating rating = Rating.builder().rating(3).feedback("updated using feignClient").build();
+//        ResponseEntity<Rating> ratingResponse = ratingService.updateRating("641c1e56d796a44f3642ba57",rating);
+//        System.out.println("STATUS CODE=>"+ratingResponse.getStatusCode() + "BODY=>"+ratingResponse.getBody());
+//    }
+
+//    @Test
+//    void deleteRating() {
+//       ratingService.deleteRating("641c1e56d796a44f3642ba57");
+//        System.out.println("STATUS CODE=>" + HttpStatus.OK + "BODY=>" + "record Deleted");
+//    }
 }
